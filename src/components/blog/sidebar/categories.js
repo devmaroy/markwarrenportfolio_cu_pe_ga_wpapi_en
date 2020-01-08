ef2d59faf73f439a81d58b575b4bf57f3a85c9ae
Marek Matejovic <me@devmaroy.cz>
2020-01-08 14:29:59 +0100

    Categories done - refactor, responsivity needed

--- src/components/blog/sidebar/categories.js
+++ src/components/blog/sidebar/categories.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import SidebarHeading from './sidebarHeading';
 
 const Categories = () => {
     return (
-        <div>
-            categories     
+        <div className="blog-sidebar-categories">
+            <SidebarHeading heading="Categories" />
+
+            <ul className="blog-sidebar-categories__list">
+                <li className="blog-sidebar-categories__item">
+                    <a href="#" className="blog-sidebar-categories__link">Web Development(12)</a>
+                </li>
+                <li className="blog-sidebar-categories__item">
+                    <a href="#" className="blog-sidebar-categories__link">UI Design(9)</a>
+                </li>
+                <li className="blog-sidebar-categories__item">
+                    <a href="#" className="blog-sidebar-categories__link">Review(6)</a>
+                </li>
+            </ul>
         </div>
     );
 };
