56f66224a60cd364f4398484b31e0c6fa45f3baa
Marek Matejovic <me@devmaroy.cz>
2020-01-08 14:46:51 +0100

    Tags done - refactor, responsivity needed

--- src/components/blog/sidebar/tags.js
+++ src/components/blog/sidebar/tags.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import SidebarHeading from './sidebarHeading';
 
 const Tags = () => {
     return (
-        <div>
-            tags   
+        <div className="blog-sidebar-tags">
+            <SidebarHeading heading="Tags" />  
+
+            <ul className="blog-sidebar-tags__list">
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Project(14)</a>
+                </li>
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Design(13)</a>
+                </li>
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Travel(11)</a>
+                </li>
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Settings(10)</a>
+                </li>
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Love(8)</a>
+                </li>
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Opinion(6)</a>
+                </li>
+                <li className="blog-sidebar-tags__item">
+                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Technology(4)</a>
+                </li>
+            </ul>
         </div>
     );
 };
