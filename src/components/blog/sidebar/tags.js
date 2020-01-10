e4a1c1494756cc740c3a1fd9b49c34ae5a27b5e5
Marek Matejovic <me@devmaroy.cz>
2020-01-10 10:56:34 +0100

    Added button small class

--- src/components/blog/sidebar/tags.js
+++ src/components/blog/sidebar/tags.js
@@ -8,25 +8,25 @@ const Tags = () => {
 
             <ul className="blog-sidebar-tags__list">
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Project(14)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Project(14)</a>
                 </li>
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Design(13)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Design(13)</a>
                 </li>
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Travel(11)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Travel(11)</a>
                 </li>
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Settings(10)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Settings(10)</a>
                 </li>
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Love(8)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Love(8)</a>
                 </li>
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Opinion(6)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Opinion(6)</a>
                 </li>
                 <li className="blog-sidebar-tags__item">
-                    <a href="#" className="button button--primary button--primary-light button--noshadow blog-sidebar-tags__link">Technology(4)</a>
+                    <a href="#" className="button button--primary button--primary-light button--noshadow button--small blog-sidebar-tags__link">Technology(4)</a>
                 </li>
             </ul>
         </div>
