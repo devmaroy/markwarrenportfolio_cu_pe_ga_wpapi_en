300a036a4d2e3c1da5c6c1c7c26abdc6e4a7269b
Marek Matejovic <me@devmaroy.cz>
2020-01-13 07:31:38 +0100

    Test styles for post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,25 +11,3 @@ const BlogPage = () => {
 };
 
 export default BlogPage;
-
-
-
-
-
-// import React from 'react';
-// import Layout from '../layout/base/layout';
-// import BlogMain from '../components/blog/blogMain.js';
-// import BlogSidebar from '../components/blog/blogSidebar.js';
-
-
-// const BlogPage = () => {
-//     return (
-//         <Layout>
-//             <BlogMain />
-//             <BlogSidebar />
-//         </Layout>
-//     );
-// };
-
-
-// export default BlogPage;
--- src/pages/post/halvah-tart-halvah-gummi-bonbon-cake-fruitcake-chocolate.js
+++ src/pages/post/halvah-tart-halvah-gummi-bonbon-cake-fruitcake-chocolate.js
@@ -1,11 +1,138 @@
 import React from 'react';
+import Layout from '../../layout/base/layout';
+import PostBase from '../../components/post/base';
+import featuredImg from '../../images/articles/article-01.jpg';
+import genericImg01 from '../../images/articles/generic-01.jpg';
+import genericImg02 from '../../images/articles/generic-02.jpg';
+
 
 const Post1 = () => {
     return (
-        <div>
-            hello there none
-        </div>
+        <Layout>
+            <PostBase>
+                <h1 className="post__title">Halvah tart halvah gummi bonbon cake fruitcake chocolate</h1>
+
+                <div className="post__meta">
+                    <ul className="post__categories">
+                        <li>
+                            <a href="#" className="post__category">UI Design</a>
+                        </li>
+                    </ul>
+
+                    <time dateTime="" className="post__date">6 hours ago</time>
+                </div>
+
+                <img src={ featuredImg } alt="" className="post__featured-img" />
+
+                <div className="post__content">
+                    <p>Macaroon cupcake dessert caramels halvah croissant sugar plum sweet roll. Cookie oat cake pastry 
+                    jujubes. Chocolate cake tootsie roll gumm bears biscuit biscuit soufflé jelly biscuit. Muffin chocolate 
+                    bar lemon drops. Cupcake cookie wafer cupcake carrot cupcake tootsie roll.  Pudding pastry dessert. 
+                    Jelly beans sweet liquorice cupcake candy. Biscuit brownie topping pudding carrot cake croissant.</p>
+
+                    <h1>Brownie sugar plum cotton</h1>
+
+                    <p>Fruitcake gingerbread sugar plum lollipop jujubes cheesecake danish brownie pie. Chocolate cake caramels liquorice sesame snaps chocolate cake. Muffin apple pie chocolate bar cake sweet candy canes. Marzipan liquorice sweet roll biscuit danish fruitcake biscuit. Topping soufflé sugar croissant chocolate bar sesame snaps jelly-o. Marzipan powder caramels donut.</p>
+
+                    <p>Oat cake wafer dessert. Dessert powder ice cream. Apple pie candy canes candy. Toffee sugar plumes 
+                    dragée oat cake ice cream chocolate. Sweet roll dragée macaroon halvah gummies macaroon carrot 
+                    cake dragée dragée. Jujubes icing candy wafer.</p>
+
+                    <h2>Halvah cheesecake lemon</h2>
+
+                    <p>Icing liquorice powder gummies jelly-o topping. Pastry jelly candy cheesecake. Cake cotton candy 
+                    cake jelly-o chocolate cake jelly sesame snaps. Tootsie roll powder topping cotton candy gummi 
+                    bears. Fruitcake cake candy canes sweet roll chupa chups icing toffee cotton candy. Sweet topping 
+                    croissant danish fruitcake. Tootsie roll gingerbread chocolate brownie marzipan pastry oat cake 
+                    lollipop biscuit.</p>
+
+                    <ul>
+                        <li><span><strong>Toffee pie marshmallow</strong> topping astry bonbon</span></li>
+                        <li><span>Marshmallow ice cream tootsie <strong>macaroon pudding</strong></span></li>
+                        <li><span>Donut wafer <strong>cotton candy</strong> cake bear claw cookie</span></li>
+                    </ul>
+
+                    <p>Brownie cake marzipan tart powder liquorice cheesecake liquorice dessert. Cake halvah caramels 
+                    chocolate chocolate. Donut liquorice tiramisu topping. Bear claw jujubes marzipan wafer lemon 
+                    drops jujubes cake jelly icing powder.</p>
+
+                    <h3>Wafer tiramisu oat cake jelly beans tootsie</h3>
+
+                    <p>Pudding tiramisu icing chocolate bar gummies dragée lollipop croissant toffee liquorice muffin 
+                    gingerbread marzipan halvah chocolate cake pastry. Apple pie soufflé chocolate bar candy canes 
+                    pudding sugar pie. Wafer soufflé halvah brownie candy canes tiramisu. Jujubes lemon powder ice 
+                    cream sugar plum powder dragée apple pie. Powder marshmallow muffin brownie dragée. Biscuit 
+                    liquorice liquorice danish bonbon cotton candy. Chocolate cake cupcake topping cupcake lemon 
+                    drops icing. Lemon drops tart bear claw jelly beans danish.</p>
+
+                    <ol>
+                        <li><span>Soufflé muffin caramels bonbon <strong>tootsie roll icing</strong></span></li>
+                        <li><span>Fruitcake <strong>sesame snaps</strong> ice cream sweet roll chocolates</span></li>
+                        <li><span><strong>Wafer caramels macaroon</strong> tiramisu apple pie lettuce</span></li>
+                    </ol>
+
+                    <p>Dessert cookie sweet roll cotton candy cake tootsie roll. Muffin pie gummi bears jelly-o lemon drops 
+                    ice cream lemon drops. Jujubes jelly apple pie. Icing powder pie candy canes. Tootsie roll tootsie roll 
+                    candy gingerbread soufflé dessert brownie sesame snaps muffin. Dessert bear claw brownie carrot 
+                    cake dessert icing macaroon fruitcake. Cake cupcake chocolate bar biscuit jujubes.</p>
+
+                    <figure>
+                        <img src={ genericImg01 } alt=""/>
+
+                        <figcaption>Danish danish jelly tiramisu liquorice</figcaption>
+                    </figure>
+
+                    <h4>Fruitcake tarts marshmallow lettuce pie</h4>
+
+
+                    <p>Lemon drops macaroon icing cupcake liquorice. Pastry topping carrot cake powder pastry topping 
+                    pie chocolate. Ice cream bonbon bonbon muffin marshmallow sweet sweet. Sugar plum danish cake 
+                    apple pie chocolate. Icing brownie marzipan jelly beans marzipan icing tiramisu.</p>
+
+                    <blockquote>
+                        <p>Dessert tiramisu cake roll Sweet lemon drops jelly beans chocolate. Sesame snaps chupa chups sweet roll chocolate cake.</p>
+                    </blockquote>
+
+                    <p>Marzipan pastry pie halvah soufflé sesame snaps. Cupcake sweet sweet. Bonbon gummies brownie 
+                    sweet. Macaroon cake lollipop tootsie roll chocolate cake. Halvah pie liquorice carrot cake gummies 
+                    pastry fruitcake powder gummi bears. Lollipop bonbon marzipan sweet roll. Topping wafer halvah. 
+                    Donut cotton candy cake sesame snaps.</p>
+
+                    <h5>Pudding cookie soufflé lettuce roll sweet tarts</h5>
+
+                    <p>Cake donut pie gummies danish cake toffee. Pastry cupcake caramels. Soufflé cotton candy lollipop 
+                    candy canes. Pie gingerbread cake. Chupa sugar plum powder caramels bonbon. Jelly marshmallow 
+                    topping croissant sweet marshmallow cake jujubes. Oat cake biscuit tart lemon drops icing soufflé. 
+                    Marzipan caramels fruitcake fruitcake candy canes liquorice cheesecake fruitcake dessert. Cake 
+                    caramels sweet candy biscuit marshmallow cookie sugar plum sesame snaps.</p>
+
+                    <img src={ genericImg02 } className="float-left" />
+                    
+                    <p>Tootsie roll toffee toffee dessert sweet lollipop tiramisu 
+                    cookie icing. Macaroon macaroon sweet roll oat bonbon. 
+                    Liquorice sweet cheesecake bonbon donut carrot brownie. 
+                    Icing candy danish chocolate bar gummi bears lollipop 
+                    sesame snaps tiramisu.</p>
+
+                    <p>Sweet roll sweet roll marshmallow chocolate cake cake. 
+                    Chocolate bar caramels gingerbread. Gummi bears powder 
+                    apple pie danish cake halvah plum. Jelly beans sweet roll 
+                    cookie pastry chocolate tiramisu. Pie chocolate cake jelly 
+                    beans cotton candy toffee. Dragée apple pie cake candy 
+                    gummies sweet roll. Sugar plum toffee sugar plum.</p>
+
+                    <p>Candy canes ice cream dragée marzipan chocolate cake macaroon chocolate cake cupcake. Bonbon ice cream cake gingerbread marshmallow marshmallow marzipan candy canes biscuit. Chocolate cake fruitcake chocolate cake ice cream jujubes. Chocolate muffin tiramisu. Topping lollipop cupcake tiramisu dragée chocolate. Candy canes chocolate cake gummies marzipan lollipop jelly-o croissant tootsie roll. Pudding candy danish caramels bear claw carrot cake tart icing. Marzipan powder biscuit pastry dragée cupcake pie. Carrot cake candy canes jujubes pudding.</p>
+
+                    <h6>Jelly beans bonbon marshmallow danish</h6>
+
+                    <p>Jelly-o sweet roll lemon drops pudding lollipop chupa chups liquorice. Soufflé donut biscuit gummi 
+                    bears cupcake lollipop. Tiramisu jelly-o tiramisu wafer marzipan tart gingerbread sugar plum jelly 
+                    jelly beans toffee toffee tiramisu. Danish cookie lemon drops muffin cupcake candy jelly-o.</p>
+                </div>
+            </PostBase>
+        </Layout>
     );
 };
+
 
 export default Post1;
