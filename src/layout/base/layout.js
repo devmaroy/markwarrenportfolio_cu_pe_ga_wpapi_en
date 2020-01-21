267de8b7116e05a9cb541fbd96658625742390de
Marek Matejovic <me@devmaroy.cz>
2020-01-21 17:20:34 +0100

    Removed import icon

--- src/layout/base/layout.js
+++ src/layout/base/layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
-import { faClock  } from '@fortawesome/free-regular-svg-icons';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../../styles/main.scss';
