d9d676c8f6d544ad2bc32b5f8cf14022ff5bfe2f
Marek Matejovic <me@devmaroy.cz>
2020-01-23 15:20:59 +0100

    Removed Link import recentArticles

--- src/components/pages/recentArticles.js
+++ src/components/pages/recentArticles.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, StaticQuery, Link } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 import Heading from '../common/heading';
 import SideArticle from '../common/sideArticle';
 
