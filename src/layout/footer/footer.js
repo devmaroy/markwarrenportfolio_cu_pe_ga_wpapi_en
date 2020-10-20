c4dc9ae19fd19f7e35a9e6e110d368614720b5c2
Marek Matejovic <me@devmaroy.cz>
2020-10-20 18:45:50 +0200

    Refactored

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import SiteLogo from '../header/siteLogo';
-import SocialLinks from '../../components/common/socialLinks';
-
+import React from "react";
+import SiteLogo from "../header/siteLogo";
+import SocialLinks from "../../components/common/socialLinks";
 
 const Footer = () => {
-    return (
-        <footer className="footer divider-space">
-            <div className="container">
-                <div className="footer__inner">
-                    <SiteLogo />
-                    
-                    <div className="footer__copyright">
-                        <p>&copy; { new Date().getFullYear() } All Rights Reserved.</p>
-                    </div>
+  return (
+    <footer className="footer divider-space">
+      <div className="container">
+        <div className="footer__inner">
+          <SiteLogo />
 
-                    <div className="footer__social">
-                        <SocialLinks type={ 'normal' } fixedWidth={ true } />
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+          <div className="footer__copyright">
+            <p>&copy; {new Date().getFullYear()} All Rights Reserved.</p>
+          </div>
+
+          <div className="footer__social">
+            <SocialLinks type={"normal"} fixedWidth={true} />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
-
 
 export default Footer;
